f743d29a4f89e68add6eebbe095cbed6da153f13
Behrooz Behamooz <behrooz.b.ir@gmail.com>
2021-04-27 19:06:35 -0700

    Updates

---
+++ precache-manifest.424444621a8368aa26abecc499492096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998bcc210df52838ebe4869ac368fd2d",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "a4f0459309a03814ab0e",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f0459309a03814ab0e",
+    "url": "/pomodoro-timer/static/js/main.ab172e92.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
